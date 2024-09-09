cb959f87598179e85742bf016b8fcef5ac17eba9
王雄皓 <x5brain@gmail.com>
2024-09-09 16:05:49 +0800

    add images

--- Package.swift
+++ Package.swift
@@ -19,9 +19,14 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HoaUIKit", dependencies: [
+            name: "HoaUIKit",
+            dependencies: [
                 "SnapKit"
-            ]),
+            ],
+            resources: [
+                .process("Sources/Assets/assets.xcassets"),
+            ]
+        ),
         .testTarget(
             name: "HoaUIKitTests",
             dependencies: ["HoaUIKit"]),
