5995e5530ad76ae9c6ef2f106473dbca4c987b3d
王雄皓 <x5brain@gmail.com>
2024-10-28 14:00:06 +0800

    fix: 读取资源异常处理

--- Sources/HoaUIKit/extensions/Bundle+Extension.swift
+++ Sources/HoaUIKit/extensions/Bundle+Extension.swift
@@ -11,27 +11,6 @@ public let imageBundle = Bundle.myModule
 private class CurrentBundleFinder {}
 extension Foundation.Bundle {
     static var myModule: Bundle = {
-        /* The name of your local package, prepended by "LocalPackages_" for iOS and "PackageName_" for macOS. You may have same PackageName and TargetName*/
-        let bundleNameIOS = "assets"
-        let candidates = [
-            /* Bundle should be present here when the package is linked into an App. */
-            Bundle.main.resourceURL,
-            /* Bundle should be present here when the package is linked into a framework. */
-            Bundle(for: CurrentBundleFinder.self).resourceURL,
-            /* For command-line tools. */
-            Bundle.main.bundleURL,
-            /* Bundle should be present here when running previews from a different package (this is the path to "…/Debug-iphonesimulator/"). */
-            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent(),
-            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
-        ]
-        
-        for candidate in candidates {
-            let bundlePathiOS = candidate?.appendingPathComponent(bundleNameIOS + ".xcassets")
-            print("bundlePathiOS \(bundlePathiOS)")
-            if let bundle = bundlePathiOS.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        fatalError("unable to find bundle")
+        .init(path: "Sources/Assets/assets.xcassets") ?? .main
     }()
 }
